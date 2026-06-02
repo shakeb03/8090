8c24c84b67b5972e41abb6122a702ddf5c76b489
Shakebuddin Mohammed <52920699+shakeb03@users.noreply.github.com>
2026-06-03 01:47:24 +0530

    Update CORS middleware to allow all methods

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origin_regex=r"https://.*\.vercel\.app",
-    allow_methods=["POST", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["Content-Type"],
 )
 
